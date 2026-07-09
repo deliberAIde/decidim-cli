f0199e74bfa1a2b4f25324d96527ea8704f6ad3d
saaltrecker <lukas.salecker@gmail.com>
2026-07-09 17:59:36 +0200

    Support GraphQL mutation field API

--- decidim-admin-api/lib/decidim/admin_api/mutation_extensions.rb
+++ decidim-admin-api/lib/decidim/admin_api/mutation_extensions.rb
@@ -4,18 +4,19 @@ module Decidim
   module AdminApi
     module MutationExtensions
       def self.included(type)
-        type.field :create_participatory_process, Decidim::AdminApi::CreateParticipatoryProcessType, null: false
-        type.field :update_participatory_process, Decidim::AdminApi::UpdateParticipatoryProcessType, null: false
-        type.field :publish_participatory_process, Decidim::AdminApi::PublishParticipatoryProcessType, null: false
-        type.field :unpublish_participatory_process, Decidim::AdminApi::UnpublishParticipatoryProcessType, null: false
-        type.field :create_process_phase, Decidim::AdminApi::CreateProcessPhaseType, null: false
-        type.field :update_process_phase, Decidim::AdminApi::UpdateProcessPhaseType, null: false
-        type.field :activate_process_phase, Decidim::AdminApi::ActivateProcessPhaseType, null: false
-        type.field :create_component, Decidim::AdminApi::CreateComponentType, null: false
-        type.field :update_component, Decidim::AdminApi::UpdateComponentType, null: false
-        type.field :publish_component, Decidim::AdminApi::PublishComponentType, null: false
-        type.field :unpublish_component, Decidim::AdminApi::UnpublishComponentType, null: false
+        type.field :create_participatory_process, mutation: Decidim::AdminApi::CreateParticipatoryProcessType, null: false
+        type.field :update_participatory_process, mutation: Decidim::AdminApi::UpdateParticipatoryProcessType, null: false
+        type.field :publish_participatory_process, mutation: Decidim::AdminApi::PublishParticipatoryProcessType, null: false
+        type.field :unpublish_participatory_process, mutation: Decidim::AdminApi::UnpublishParticipatoryProcessType, null: false
+        type.field :create_process_phase, mutation: Decidim::AdminApi::CreateProcessPhaseType, null: false
+        type.field :update_process_phase, mutation: Decidim::AdminApi::UpdateProcessPhaseType, null: false
+        type.field :activate_process_phase, mutation: Decidim::AdminApi::ActivateProcessPhaseType, null: false
+        type.field :create_component, mutation: Decidim::AdminApi::CreateComponentType, null: false
+        type.field :update_component, mutation: Decidim::AdminApi::UpdateComponentType, null: false
+        type.field :publish_component, mutation: Decidim::AdminApi::PublishComponentType, null: false
+        type.field :unpublish_component, mutation: Decidim::AdminApi::UnpublishComponentType, null: false
       end
     end
   end
 end
+
